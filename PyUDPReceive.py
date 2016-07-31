5341e7ed955e6a261d02a3738010138a02f3a00f
Dan White <danw@pipeworks.com>
2016-07-30 22:41:14 -0700

    Add counter.

--- PyUDPReceive.py
+++ PyUDPReceive.py
@@ -6,6 +6,10 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind(('', UDP_PORT))
 
+num = 0
+
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    num += 1
+    print num
     print "received message:", data
