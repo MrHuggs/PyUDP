e9fc32ce9ce4881c52e1bf2fb7fd816a4951c77e
Dan White <danw@pipeworks.com>
2016-08-14 19:59:38 -0700

    Fix & Test joystick controls.

--- JoystickCtrl.py
+++ JoystickCtrl.py
@@ -1,6 +1,6 @@
-"""Example implementation of joystick control for gopigo."""
+﻿"""Example implementation of joystick control for gopigo."""
 
-from UDPController import UDPController
+from PyUDPReceive import UDPController
 import gopigo
 
 controller = UDPController()
@@ -18,13 +18,19 @@ while(True):
                 right_speed -= (left_speed - 255)
                 left_speed -= (left_speed - 255)
         else:
-            left_speed = 255*max(abs(move_value), rotate_value)
-            right_speed = 255*abs(move_value) + rotate_value
+            left_speed = 255*max(abs(move_value), abs(rotate_value))
+            right_speed = 255*(abs(move_value) + rotate_value)
             if(right_speed > 255):
                 left_speed -= (right_speed - 255)
                 right_speed -= (right_speed - 255)
-        gopigo.set_left_speed(left_speed)
-        gopigo.set_right_speed(right_speed)
+
+        if left_speed != 0 or right_speed != 0:
+            print "controls: ({0}, {1})".format(controller.x, controller.y)
+            dir = "forward" if move_value >= 0  else "back"
+            print "{0} left:{1} right:{2}".format(dir, left_speed, right_speed)
+
+        gopigo.set_left_speed(int(left_speed))
+        gopigo.set_right_speed(int(right_speed))
         if (move_value > 0):
             gopigo.fwd()
         else:
