c6624eb79c0b7d83da4f1652051c86451d88a1bf
daniwhite <white.dani.m@gmail.com>
2016-08-14 20:14:05 -0700

    Change method for setting motors

--- JoystickCtrl.py
+++ JoystickCtrl.py
@@ -2,8 +2,21 @@
 
 from PyUDPReceive import UDPController
 import gopigo
+from math import copysign
 
 controller = UDPController()
+
+
+def calc_speed(move_value, rotate_value):
+    """Calculate motor speeds based on joystick inputs"""
+    left_speed, right_speed = abs(move_value)
+    left_speed += rotate_value
+    right_speed -= rotate_value
+    right_speed = min(max(right_speed, -1), 1)
+    left_speed = min(max(left_speed, -1), 1)
+    left_speed *= copysign(255, move_value)
+    right_speed *= copysign(255, move_value)
+    return [left_speed, right_speed]
 
 while(True):
     left_speed = 0
@@ -11,27 +24,20 @@ while(True):
     if controller.read():
         move_value = controller.y
         rotate_value = controller.x
-        if rotate_value > 0:
-            left_speed = 255*(abs(move_value) + rotate_value)
-            right_speed = 255*max(abs(move_value), rotate_value)
-            if(left_speed > 255):
-                right_speed -= (left_speed - 255)
-                left_speed -= (left_speed - 255)
-        else:
-            left_speed = 255*max(abs(move_value), abs(rotate_value))
-            right_speed = 255*(abs(move_value) + rotate_value)
-            if(right_speed > 255):
-                left_speed -= (right_speed - 255)
-                right_speed -= (right_speed - 255)
-
+        [left_speed, right_speed] = calc_speed(move_value, rotate_value)
         if left_speed != 0 or right_speed != 0:
             print "controls: ({0}, {1})".format(controller.x, controller.y)
             dir = "forward" if move_value >= 0  else "back"
             print "{0} left:{1} right:{2}".format(dir, left_speed, right_speed)
-
-        gopigo.set_left_speed(int(left_speed))
-        gopigo.set_right_speed(int(right_speed))
-        if (move_value > 0):
-            gopigo.fwd()
+        gopigo.set_left_speed(int(abs(left_speed)))
+        gopigo.set_right_speed(int(abs(right_speed)))
+        if left_speed > 0:
+            if right_speed > 0:
+                gopigo.fwd()
+            else:
+                gopigo.right_rot()
         else:
-            gopigo.bwd()
+            if right_speed > 0:
+                gopigo.left_rot()
+            else:
+                gopigo.bwd()
